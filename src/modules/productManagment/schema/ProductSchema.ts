0b4156c37a8c35e2f37155b967764a99b42a3cef
rfbatista <renfbatista@gmail.com>
2021-10-20 22:57:15 -0300

    feat: new container lib and schemas

--- src/modules/productManagment/schema/ProductSchema.ts
+++ src/modules/productManagment/schema/ProductSchema.ts
@@ -14,7 +14,7 @@ import { BarCode } from '../entities/BarCode';
 import { Category } from '../entities/Category';
 import { Cost } from '../entities/Cost';
 import { Price } from '../entities/Price';
-import { IProduct } from '../entities/Product';
+import { IProduct, Product } from '../entities/Product';
 import { Tag } from '../entities/Tag';
 import { CostSchema } from './CostSchema';
 import { PriceSchema } from './PriceSchema';
@@ -23,8 +23,23 @@ import { ImageSchema } from './ImageSchema';
 import { OrganizationSchema } from '@modules/organizationManagment/schemas/OrganizationSchema';
 import { CategorySchema } from './CategorySchema';
 
+export interface IProductSchema {
+  id?: number;
+  organization: OrganizationSchema;
+  title: string;
+  is_published: boolean;
+  price: PriceSchema;
+  categories?: CategorySchema[];
+  images?: ImageSchema[];
+  BarCode?: string;
+  cost?: CostSchema;
+  description?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  deleted_at?: Date;
+}
 @Entity('product')
-export class ProductSchema {
+export class ProductSchema implements IProductSchema {
   @PrimaryColumn()
   id?: number;
 
@@ -37,13 +52,20 @@ export class ProductSchema {
   @Column()
   is_published: boolean;
 
-  @OneToMany(type => PriceSchema, price => price.product)
+  @OneToMany((type) => PriceSchema, (price) => price.product, {
+    cascade: ['insert', 'remove'],
+    nullable: false,
+    eager: true
+  })
   price: PriceSchema;
 
   @Column()
   description?: string;
 
-  @ManyToMany((type) => Category)
+  @ManyToMany((type) => Category, {
+    nullable: true,
+    eager: true
+  })
   @JoinTable({
     name: 'product_categories',
     joinColumn: { name: 'product', referencedColumnName: 'id' },
@@ -51,19 +73,38 @@ export class ProductSchema {
   })
   categories?: CategorySchema[];
 
-  @OneToMany(type => ImageSchema, image => image.product)
-  images?: { description: string; url: string }[];
+  @OneToMany((type) => ImageSchema, (image) => image.product, {
+    nullable: true,
+  })
+  images?: ImageSchema[];
 
   @Column()
-  barCode?: BarCode;
+  barCode?: string;
 
   @ManyToMany(() => TagSchema, (tags) => tags.product, {
-    cascade: false,
+    cascade: ['insert'],
+    nullable: true,
+    eager: true
   })
-  @JoinTable()
+  @JoinTable({
+    name: 'product_categories', // table name for the junction table of this relation
+    joinColumn: {
+      name: 'productId',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'categoryId',
+      referencedColumnName: 'id',
+    },
+  })
   tags?: TagSchema[];
 
-  @OneToMany(() => CostSchema, (cost) => cost.product)
+  @OneToMany(() => CostSchema, (cost) => cost.product, {
+    onDelete: 'CASCADE',
+    cascade: ['insert', 'remove'],
+    nullable: true,
+    eager: true
+  })
   cost?: CostSchema;
 
   @CreateDateColumn()
@@ -74,4 +115,25 @@ export class ProductSchema {
 
   @DeleteDateColumn()
   deleted_at?: Date;
+
+  constructor(props: IProductSchema, id?: number) {
+    Object.assign(this, {...props, id})
+  }
+
+  toEntity(): Product {
+    return Product.create({
+      organization: null,
+      title: this.title,
+      isPublished: this.is_published,
+      price: new PriceSchema(this.price).toEntity(),
+      description: this.description,
+      categories: 
+      images?: Image[];
+      barCode?: BarCode;
+      tags?: Tag[];
+      cost?: Cost;
+      created_at?: Date;
+      updated_at?: Date;
+    }, this.id).getValue()
+  }
 }
